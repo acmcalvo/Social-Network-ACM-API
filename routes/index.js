5d96a6b47e6515de4457cc5239d929f35ecc2b05
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2022-02-19 17:28:00 -0500

    create utils dateFormat.js

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const router = require("express").Router();
 //import routes from /api/index.js
 const apiRoutes = require("./api");
 
-//add api prefix to imported routes
+// add prefix of `/api` to all of the api routes imported from the `api` directory
 router.use("/api", apiRoutes);
 
 router.use((req, res) => {
-  res.status(404).send("<h1>😝 404 Error!</h1>");
+  res.status(404).send("<h1> 404 Error!</h1>");
 });
 
 module.exports = router;
